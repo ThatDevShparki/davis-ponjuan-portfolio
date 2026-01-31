8f21951f874adcdbe346fee4b732adb93a376d0a
ThatDevShparki <shparki@thatdev.io>
2026-01-31 08:31:39 -0600

    chore: remove test file

--- test-hook.ts
+++
@@ -1,8 +0,0 @@
-// Intentionally poorly formatted file to test pre-commit hook
-
-const badlyFormatted = () => {
-  const unused = 'this should be detected';
-  return 'test';
-};
-
-export { badlyFormatted };
